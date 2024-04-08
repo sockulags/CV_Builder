4cb142dce1b142da4d85aaff9817251e0df8b424
Lucas Skog <lucasskog@gmail.com>
2024-04-08 15:13:09 +0200

    Update

--- src/config/firebase.ts
+++ src/config/firebase.ts
@@ -2,14 +2,17 @@ import { initializeApp } from "firebase/app";
 import { getAuth, GoogleAuthProvider } from "firebase/auth";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyBtHaDhubmJprcqKDjS9p5LnxspXIYFOqM",
-  authDomain: "cvbuilder-bd640.firebaseapp.com",
-  projectId: "cvbuilder-bd640",
-  storageBucket: "cvbuilder-bd640.appspot.com",
-  messagingSenderId: "1044068599738",
-  appId: "1:1044068599738:web:b5a6a0f85a38c87e25ff83",
-  measurementId: "G-NXD4QD6VRJ"
-};
+    apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
+    authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
+    projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
+    storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
+    messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
+    appId: process.env.REACT_APP_FIREBASE_APP_ID,
+    measurementId: process.env.REACT_APP_FIREBASE_MEASUREMENT_ID
+  };
+  
+
+
 
 const app = initializeApp(firebaseConfig);
 // const analytics = getAnalytics(app);
